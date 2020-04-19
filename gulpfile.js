081d5847e21bec1d190c8de31d20b077531bb883
Simple <591831866@qq.com>
2020-04-19 10:45:45 +0800

    chore:使用 async/await 处理异步任务

--- gulpfile.js
+++ gulpfile.js
@@ -37,65 +37,67 @@ function css(cb) {
 }
 
 // 处理 JS
-function script(cb) {
-  // rollup 打包 js 模块
-  rollup.rollup({
-    // 入口文件
-    input: './src/js/index.js',
-    plugins: [
-      resolve(),
-      babel({
-        exclude: 'node_modules/**' // only transpile our source code
-      })
-    ]
-  }).then(bundle => {
-    bundle.write({
+async function script(cb) {
+  try {
+    const bundle = await rollup.rollup({
+      input: './src/js/index.js',
+      plugins: [
+        resolve(),
+        babel({
+          exclude: 'node_modules/**' // 不处理 node_modules
+        })
+      ]
+    })
+    await bundle.write({
       // iife 规范下的全局变量名称
       name: 'wangEditor',
       // 产出的未压缩的文件名
       file: './release/wangEditor.js',
       // 产出文件使用 umd 规范（即兼容 amd cjs 和 iife）
       format: 'umd'
-    }).then(() => {
-      // 待 rollup 打包 js 完毕之后，再进行如下的处理：
-      src('./release/wangEditor.js')
-        // 压缩
-        .pipe(uglify())
-        // 产出的压缩的文件名
-        .pipe(rename('wangEditor.min.js'))
-        // 生成 sourcemap
-        .pipe(dest('./release'))
-      cb()
     })
-  })
+
+    // 待 rollup 打包 js 完毕之后，再进行如下的处理：
+    src('./release/wangEditor.js')
+      // 压缩
+      .pipe(uglify())
+      // 产出的压缩的文件名
+      .pipe(rename('wangEditor.min.js'))
+      // 生成 sourcemap
+      .pipe(dest('./release'))
+  } catch (e) {
+    console.error(e)
+  }
+
+  cb()
 }
 
-function transpileJS(cb) {
-  // rollup 打包 js 模块
-  rollup.rollup({
-    // 入口文件
-    input: './src/js/index.js',
-    plugins: [
-      resolve(),
-      babel({ exclude: 'node_modules/**' })
-    ]
-  }).then(bundle => {
-    bundle.write({
+async function transpileJS(cb) {
+  try {
+    const bundle = await rollup.rollup({
+      input: './src/js/index.js',
+      plugins: [
+        resolve(),
+        babel({ exclude: 'node_modules/**' })
+      ]
+    })
+
+    await bundle.write({
       name: 'wangEditor',
       file: './release/wangEditor.js',
       format: 'umd'
-    }).then(cb).catch(cb)
-  }).catch(cb)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+  cb()
 }
 
 function transpileCSS(cb) {
   src('./src/less/**/*.less')
     .pipe(less())
     .pipe(concat('wangEditor.css'))
-    .pipe(postcss([
-      autoprefixer,
-      cssgrace
-    ]))
+    .pipe(postcss([autoprefixer, cssgrace]))
     .pipe(dest('./release'))
   cb()
 }
@@ -105,10 +107,15 @@ function watchFileChange(cb) {
   watch('./src/js/**/*.js', transpileJS)
   // 监听 css 原始文件的变化
   watch('./src/less/**/*.less', transpileCSS)
+
   cb()
 }
 
 exports.watch = watchFileChange
+
+exports.transpileCSS = transpileCSS
+
+exports.transpileJS = transpileJS
 
 // 默认任务配置
 exports.default = series(copyFonts, css, script)
